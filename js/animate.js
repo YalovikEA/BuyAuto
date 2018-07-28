4cde7fb6d940233ea29b895e87e24527eeffa112
Evgeny Yalovik <yalovikea@gmail.com>
2018-07-28 15:09:52 +0300

    Изменено появление анимации

--- js/animate.js
+++ js/animate.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
-  $('.sale__title').animated('fadeInDown', 'fadeOutUp');
-  $('.sale__sub-title').animated('fadeInUp', 'fadeOutDown');
-  $('.section-title').animated('fadeInUp', 'fadeOutDown');
-  $('.work__sub-title').animated('fadeInUp', 'fadeOutDown');
-  $('.assess__sub-title').animated('fadeInUp', 'fadeOutDown');
-  $('.work__list').animated('flipInY', 'fadeOutDown');
-  $('.bonus__item').animated('rollIn', 'rollOut');
-  $('.slider').animated('zoomIn', 'zoomOut');
-  $('.feature').animated('rollIn', 'rollOut');
+  if (  jQuery(window).width() > 768 ) {
+    $('.sale__title').animated('fadeInDown', 'fadeOutUp');
+    $('.sale__sub-title').animated('fadeInUp', 'fadeOutDown');
+    $('.section-title').animated('fadeInUp', 'fadeOutDown');
+    $('.work__sub-title').animated('fadeInUp', 'fadeOutDown');
+    $('.assess__sub-title').animated('fadeInUp', 'fadeOutDown');
+    $('.work__list').animated('flipInY', 'fadeOutDown');
+    $('.bonus__item').animated('rollIn', 'fadeOutDown');
+    $('.slider').animated('zoomIn', 'fadeOutDown');
+    $('.feature').animated('rollIn', 'fadeOutDown');
+
+}
 
 });
